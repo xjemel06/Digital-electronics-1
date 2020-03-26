8b1f1e93f57910993e4c27a887cf2034edc768d6
xjemel06 <60607239+xjemel06@users.noreply.github.com>
2020-03-26 16:00:29 +0100

    Update testbench.vhd

--- Labs/08-traffic_lights/testbench.vhd
+++ Labs/08-traffic_lights/testbench.vhd
@@ -1,59 +1,91 @@
-LIBRARY ieee;
-use ieee.std_logic_1164.ALL;
-use ieee.numeric_std.all;
+-- Code your testbench here
+-----------------------------------------------------------------------------------library declaration
+---------------------------------------------------------------------------------
 
-ENTITY testbench IS
-END testbench;
- 
-ARCHITECTURE tb OF testbench IS 
- 
-    COMPONENT top_traffic
-    PORT(
-    clk_i    : in  std_logic;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+----------------------------------------------------------------------------------- Entity declaration
+---------------------------------------------------------------------------------
+
+entity testbench is
+--empty
+end testbench;
+
+architecture tb of testbench is
+  -- DUT component
+  component traffic_lights is
+  port(
+	-- INPUTS
+	clk_i    : in  std_logic;
     srst_n_i : in  std_logic;   -- Synchronous reset (active low)
+	ce_2Hz_i : in std_logic;	-- clock enable -- timing
+    -- OUTPUTS
+    lights_o  : out unsigned(6-1 downto 0)
     
-	LED_o	 : out std_logic_vector(5 downto 0)
-	);
-    END COMPONENT;
+    -- lights_o(5) - red traffic light 1
+    -- lights_o(4) - yellow traffic light 1
+    -- lights_o(3) - green traffic light 1
     
-	-- inputs
-    signal clk_i    : std_logic;
-    signal srst_n_i : std_logic;     
-   -- outputs
-	signal LED_o	: std_logic_vector(5 downto 0);
-
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   UUT: top_traffic PORT MAP (clk_i => clk_i,
-                                srst_n_i => srst_n_i,
-                                LED_o =>LED_o
-   								);
-
-
-	Clk_gen: process	-- NEW
+    
+    -- lights_o(2) - red traffic light 2
+    -- lights_o(1) - yellow traffic light 2
+    -- lights_o(0) - green traffic light 2
+  );
+  end component;
+  --INPUTS
+  signal clk_in   	: std_logic := '0'; 
+  signal srst_n_in 	: std_logic := '0';   
+  signal ce_2Hz_in	: std_logic := '0';
+  
+  --OUTPUT
+  signal lights_out	: unsigned(6-1 downto 0 );
+  
+  BEGIN
+	UUT: traffic_lights port map(
+      clk_i => clk_in,
+      srst_n_i => srst_n_in,
+      ce_2Hz_i => ce_2Hz_in,
+      lights_o => lights_out 
+    );
+	
+   -- Clock process definitions
+	Clk_gen: process	
   	begin
-    	while Now < 5000 NS loop		-- NEW: DEFINE SIMULATION TIME
-      		clk_i <= '0';
+    	while Now < 500 nS loop
+      		clk_in <= '0';
       		wait for 0.5 NS;
-      		clk_i <= '1';
+      		clk_in <= '1';
       		wait for 0.5 NS;
     	end loop;
     	wait;
   	end process Clk_gen;
-
-    -- Stimulus process
-    stim_proc: process
-    begin
-        srst_n_i <= '1';
-        wait for 500 NS;
-        srst_n_i <= '0';
-        wait until rising_edge(clk_i);
-        wait until rising_edge(clk_i);
-        wait until rising_edge(clk_i);
-        srst_n_i <= '1';
-        wait;
-    end process;
-
+    
+    Clk_2Hz_gen: process	
+  	begin
+    	while Now < 500 nS loop
+      		ce_2Hz_in <= '0';
+      		wait for 0.5 NS;
+      		ce_2Hz_in <= '1';
+      		wait for 0.5 NS;
+    	end loop;
+    	wait;
+  	end process Clk_2Hz_gen;
+   
+   -- Stimulus process
+   stim_proc: process
+   begin	
+   	  
+      srst_n_in <= '1';
+      wait until rising_edge(clk_in);
+      wait until rising_edge(clk_in);
+      srst_n_in <= '0';
+      wait until rising_edge(clk_in);
+      wait until rising_edge(clk_in);
+      wait until rising_edge(clk_in);
+      srst_n_in <= '1';
+      
+      wait;
+   end process;
 end tb;
